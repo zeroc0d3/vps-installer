d25dc124a4b27fdad991e979c6d7ab60c24d0d74
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2018-01-30 00:14:49 +0700

    Add default value host_ip & host_name

--- configure_hosts.sh
+++ configure_hosts.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env sh
 
+##### DEFAULT VALUE (CONFIG) #####
+if [ -z "$HOST_IP" ] 
+then
+  HOST_IP='192.168.0.10'
+fi 
+
+if [ -z "$HOST_NAME" ] 
+then
+  HOST_NAME='zeroc0d3lab.io'
+fi 
+
 ##### SETUP HOST & HOSTNAME###
 sudo echo "$HOST_IP   $HOST_NAME" >> /etc/hosts
 sudo sysctl kernel.hostname=$HOST_NAME
