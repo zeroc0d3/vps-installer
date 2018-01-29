13cb096187fbedfb9a1da7ff75f5b8e86ca9e372
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2018-01-30 00:14:22 +0700

    Add default value password for user deploy

--- change_password.sh
+++ change_password.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+##### DEFAULT VALUE (CONFIG) #####
+if [ -z "$DEPLOY_ACCOUNT" ] 
+then
+  DEPLOY_ACCOUNT='deploy'
+fi 
+
 ##### CHANGE DEPLOY PASSWORD #####
 # sudo echo "$DEPLOY_ACCOUNT:$PASSWORD_DEPLOY" | /usr/sbin/chgpasswd 
 sudo passwd $DEPLOY_ACCOUNT
