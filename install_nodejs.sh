5946c5a706d8f7771bf34999177bd9422e1b78ac
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2018-01-30 00:16:18 +0700

    Add default value nvm and path installation node_js

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env sh
 
+##### DEFAULT VALUE (CONFIG) #####
+if [ -z "$PATH_HOME" ] 
+then
+  PATH_HOME=$HOME
+fi 
+
+if [ -z "$NVM_VERSION" ] 
+then
+  NVM_VERSION='8.9.4'
+fi 
+
 ##### INSTALL NODEJS #####
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
 
